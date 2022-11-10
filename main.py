b4a9757a0f60ff235ddc3672e5eb9e5ce4fe6164
junge8971 <99283906+junge8971@users.noreply.github.com>
2022-11-10 13:26:24 +0300

    Added the function to add records within a month

--- main.py
+++ main.py
@@ -5,15 +5,8 @@ from bs4 import BeautifulSoup
 import requests
 import psycopg2
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from threading import Thread
-
-
-def main():
-    a = WeatherParser()
-    TR1 = Thread(target=a.years_iterating, args=(2012, 2))
-    TR2 = Thread(target=a.years_iterating, args=(2013, 2))
-    TR1.start()
-    TR2.start()
 
 
 class WeatherParser:
@@ -21,19 +14,29 @@ class WeatherParser:
         self.calendar = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         self.leap_years = [2024, 2020, 2016, 2012]
         self.current_date = datetime.now()
-        self.reference_url = 'http://www.pogodaiklimat.ru/weather.php?id=34927&bday=1&fday={}&amonth={}&ayear={}&bot=2'
+        self.start_year = 2012
+        self.reference_url = 'http://www.pogodaiklimat.ru/weather.php?id=34927&bday={}&fday={}&amonth={}&ayear={}&bot=2'
 
-    def years_iterating(self, start_year=2012, step_fro_iteration=1):
+    def years_iterating(self, start_year: int = 2012,
+                        step_fro_iteration: int = 1,
+                        start_month: int = 1,
+                        start_day: int = 1,
+                        end_day: int = None):
         for year in range(start_year, int(self.current_date.year)+1, step_fro_iteration):
-            for month in range(1, 13):
+            for month in range(start_month, 13):
                 if year in self.leap_years and month == 2:
-                    current_url = self.reference_url.format(29, month, year)
+                    current_url = self.reference_url.format(start_day, 29, month, year)
                     self.weather_parsing(current_url, year)
                 else:
-                    current_url = self.reference_url.format(self.calendar[month - 1], month, year)
-                    self.weather_parsing(current_url, year)
+                    print('ok4')
+                    if end_day is not None:
+                        current_url = self.reference_url.format(start_day, end_day, month, year)
+                        self.weather_parsing(current_url, year)
+                    else:
+                        current_url = self.reference_url.format(start_day, self.calendar[month - 1], month, year)
+                        self.weather_parsing(current_url, year)
 
-    def weather_parsing(self, url, year):
+    def weather_parsing(self, url: str, year: int):
         page = requests.get(url)
         page.encoding = 'utf-8'
         page2 = page.text
@@ -43,6 +46,7 @@ class WeatherParser:
         time_table = archive_table.find('div', {'class': 'archive-table-left-column'}).table
         tr_from_time_table = []
         td_from_time_table = time_table.findAll('td', {'class': 'black'})
+
         list_for_time = []
         list_for_date = []
         i = 0
@@ -68,18 +72,17 @@ class WeatherParser:
                 x = str(td).replace('\n', '')
                 if x != '':
                     appending_text.append(td.text)
-            insert.insert_new_row(self.convert_items_to_float(appending_text, year), year)
+            insert.insert_new_row(self.convert_items_to_float(appending_text, year), 'y'+str(year))
             appending_text = []
 
-    # to do: в поле с датой в 10-ом месяце пропадает ноль
-    def convert_items_to_float(self, item_list, year):
+    def convert_items_to_float(self, item_list: list, year: int):
         result_list = []
         counter = 0
         while counter < len(item_list):
             item = item_list[counter]
             if counter == 1:
-                day, mounth = item.split('.')
-                item = f'{year}-{mounth}-{day}'
+                day, month = item.split('.')
+                item = f'{year}-{month}-{day}'
             try:
                 result_list.append(float('{0:.2f}'.format(item)))
             except ValueError:
@@ -91,6 +94,36 @@ class WeatherParser:
         print(result_list)
         return result_list
 
+    def update_weather_data(self):
+        connect_to_bd = Database()
+        created_tables = connect_to_bd.checking_for_tables()
+
+        list_of_years = []
+        for item in created_tables:
+            list_of_years.append(item[0])
+
+        last_received_day_dirt = connect_to_bd.last_received_day(list_of_years[-1])
+        last_received_date = last_received_day_dirt[0][0]
+        print(last_received_date)
+
+        if last_received_date != self.current_date.date():
+            delta_time = relativedelta(self.current_date.date(), last_received_date)
+            if delta_time.months == 0 and delta_time.years == 0:
+                print('ok1')
+                if last_received_date.day == self.calendar[last_received_date.month+1]:
+                    print('ok2')
+                    self.years_iterating(start_year=self.current_date.year,
+                                         start_month=self.current_date.month,
+                                         end_day=self.current_date.day-1)
+                else:
+                    print('ok3')
+                    self.years_iterating(start_year=self.current_date.year,
+                                         start_month=self.current_date.month,
+                                         start_day=last_received_date.day,
+                                         end_day=self.current_date.day - 1)
+        else:
+            return 'No need to update'
+
 
 class Database:
     def __init__(self):
@@ -99,7 +132,7 @@ class Database:
         self.db_password = '123321'
         self.db_host = '127.0.0.1'
 
-    def request_to_bd(self, reqvest_to_db, params=None):
+    def request_to_bd(self, reqvest_to_db: str, params: list = None):
         try:
             connect = psycopg2.connect(dbname=self.db_name,
                                        user=self.db_user,
@@ -109,12 +142,12 @@ class Database:
             if params is None:
                 cursor.execute(reqvest_to_db)
                 connect.commit()
-                record = cursor.fetchone()
+                record = cursor.fetchall()
             else:
                 full_reqvest = cursor.mogrify(reqvest_to_db, params)
                 cursor.execute(full_reqvest)
                 connect.commit()
-                record = cursor.fetchone()
+                record = cursor.fetchall()
             return record
         except Exception as exept:
             print(f'Ошибка подключения: {exept}')
@@ -124,16 +157,15 @@ class Database:
                 connect.close()
                 print('База данных закрыта')
 
-    def insert_new_row(self, text_to_insert, year):
-        current_year = 'y'+str(year)
+    def insert_new_row(self, text_to_insert: list, year: str):
         text = """INSERT INTO {} VALUES
         (
         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s 
-        )""".format(current_year)
+        )""".format(year)
         self.request_to_bd(text, params=text_to_insert)
 
-    def crating_new_table(self, year):
+    def crating_new_table(self, year: str):
         text = """
         CREATE TABLE IF NOT EXISTS {}
         (
@@ -162,6 +194,42 @@ class Database:
         """.format(year)
         return self.request_to_bd(text)
 
-    
+    def checking_for_tables(self) -> list:
+        text = """
+            SELECT table_name FROM information_schema.tables
+            WHERE table_schema NOT IN ('information_schema','pg_catalog')
+            ORDER BY table_name;"""
+        return self.request_to_bd(text)
+
+    def last_received_day(self, year: str) -> str:
+        text = """
+        SELECT date FROM {} 
+        ORDER BY date DESC
+        limit 1;
+        
+        """.format(year)
+        return self.request_to_bd(text)
+
+
+def main():
+    while True:
+        command = input('>>> ')
+        if command == 'parse':
+            a = WeatherParser()
+            b = WeatherParser()
+            tr1 = Thread(target=a.years_iterating, args=(2012, 2))
+            tr2 = Thread(target=b.years_iterating, args=(2013, 2))
+            tr1.start()
+            tr2.start()
+        elif command == 'update':
+            a = WeatherParser()
+            a.update_weather_data()
+        else:
+            print("""
+            parse - спарсить всё
+            update - обновить уже имеющиеся данные
+            """)
+
+
 if __name__ == '__main__':
     main()
