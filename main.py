6a48a9b55c9d134f739a71ec6a778152db010612
junge8971 <99283906+junge8971@users.noreply.github.com>
2023-05-15 15:45:53 +0300

    Прописана маршрутизация для для главной страницы

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.secret_key = 'asfouhvnsldvszkldvmszdfv'
+
+
+@app.route('/')
 def main():
-    pass
+
+    return render_template('today.html')
 
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', debug=True)
