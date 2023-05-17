b93733d2a9cdeb132b5d57686ddc698b646eb77d
junge8971 <99283906+junge8971@users.noreply.github.com>
2023-05-17 11:52:57 +0300

    Добвлена страница погоды за неделю. Функции по созданию json для графиков и для шаблока вынесены отдельно

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import pandas
 from flask import Flask, render_template
 
 from datetime import datetime, timedelta
@@ -29,26 +29,74 @@ def main():
 
     data_frame = pd.DataFrame.from_records(current_weather)
 
+    graphs = make_graphs(data_frame)
+
+    return render_template('today.html', weather=current_weather, time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                           graph_temp=graphs.get('temp'), graph_wind=graphs.get('wind'), graph_wet=graphs.get('wet'))
+
+
+@app.route('/week')
+def week():
+    current_date = datetime.now().date()
+    current_week_day = current_date.weekday()
+    start_date = current_date - timedelta(days=current_week_day)
+    db = Database()
+    week_weather = db.get_weather_json(str(start_date), str(current_date))
+
+    for item in week_weather:
+        item['date_and_time_utc'] = item['date_and_time_utc'].replace('T', ' ').replace('+03:00', '')
+        if item['conditions'] is None:
+            item['conditions'] = 'Нет'
+
+    data_frame = pd.DataFrame.from_records(week_weather)
+
+    weather = make_json_with_mean_data(data_frame)
+
+    graphs = make_graphs(data_frame)
+
+    return render_template('week_template.html', weather=weather, time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                           graph_temp=graphs.get('temp'), graph_wind=graphs.get('wind'), graph_wet=graphs.get('wet'))
+
+
+def make_graphs(data_frame: pandas.DataFrame) -> dict[str:str]:
     fig_temp = px.line(data_frame=data_frame, y='t', x='date_and_time_utc',
-                       title='График температуры за сегодня')
-    fig_temp.update_layout(xaxis_title="Время", yaxis_title="Температура",)
+                       title='График температуры')
+    fig_temp.update_layout(xaxis_title="Время", yaxis_title="Температура", )
     graph_temp = json.dumps(fig_temp, cls=plotly.utils.PlotlyJSONEncoder)
 
     fig_wind = px.line(data_frame=data_frame, y='wind_spead', x='date_and_time_utc',
-                       title='График скорости ветра за сегодня')
+                       title='График скорости ветра')
     fig_wind.update_layout(xaxis_title="Время", yaxis_title="Скорость ветра, м/с")
     graph_wind = json.dumps(fig_wind, cls=plotly.utils.PlotlyJSONEncoder)
 
     fig_wet = px.line(data_frame=data_frame, y='f', x='date_and_time_utc',
-                       title='График влажности за сегодня')
+                      title='График влажности')
     fig_wet.update_layout(xaxis_title="Время", yaxis_title="Влажность воздуха, %")
     graph_wet = json.dumps(fig_wet, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('today.html', weather=current_weather, time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                           graph_temp=graph_temp, graph_wind=graph_wind, graph_wet=graph_wet)
+    return {'temp': graph_temp, 'wind': graph_wind, 'wet': graph_wet}
+
+
+def make_json_with_mean_data(data_frame: pandas.DataFrame) -> dict:
+    t = data_frame['t'].mean()
+    f = data_frame['f'].mean()
+    wind_direction = data_frame['wind_direction'].value_counts().index[0]
+    data_frame['wind_spead'] = pd.to_numeric(data_frame['wind_spead'], downcast='float')
+    wind_spead = data_frame['wind_spead'].mean()
+    cloudiness = data_frame['cloudiness'].value_counts().index[0]
+    comfort = data_frame['comfort'].value_counts().index[0]
+    conditions = data_frame['conditions'].value_counts().index[0]
+    weather = {
+        't': int(t),
+        'f': int(f),
+        'wind_direction': wind_direction,
+        'wind_spead': int(wind_spead),
+        'cloudiness': cloudiness,
+        'comfort': comfort,
+        'conditions': conditions,
+    }
+    return weather
 
 
 if __name__ == '__main__':
-    update = WeatherParser()
-    update.update_weather_data()
     app.run(host='0.0.0.0', debug=True)
